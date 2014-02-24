8c9e0469f3512acb8c1df288f87e32febaed85ce
Marc MAURICE <marc-github@pub.positon.org>
2014-02-24 19:55:24 +0100

    Add pop sound on volume change with libcanberra

---
+++ src/popsound.h
@@ -0,0 +1,15 @@
+/*
+ * This file is part of pa-applet.
+ *
+ * Refer to the LICENSE file for licensing information.
+ *
+ */
+
+#ifndef POPSOUND_H
+#define POPSOUND_H
+
+void popsound_init(void);
+void popsound_destroy(void);
+void popsound_play(void);
+
+#endif
